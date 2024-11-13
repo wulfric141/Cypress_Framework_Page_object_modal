a84b67bdc05d11e17f2b84981e1d7688280d37ee
Abhishek Singh <abhishekgps006@gmail.com>
2024-11-13 14:43:34 +0530

    added code

--- cypress/e2e/dbtest.cy.js
+++ cypress/e2e/dbtest.cy.js
@@ -1,12 +1,12 @@
 
 describe('Database Query', () => {
-    const sqlquery='SELECT firstname from persons where personid=1';
+    const sqlquery='SELECT firstname from persons';
    
       it.only("query for user abhi",()=>{
         cy.task('queryDb',sqlquery).then((result) => {
-          cy.log(result)
           result.forEach((row, index) => {
             cy.log(`Row ${index + 1}: ${JSON.stringify(row)}`);
+            
           
   
       })
